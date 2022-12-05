0c02280dc59feb55a22fc4f1172c38ea0eaf03fd
kody-oneill <kody.oneill@yahoo.com>
2022-12-05 15:49:46 -0600

    First commit

---
+++ testapp/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'testapp for Kody';
+  message = 'Kody\'s message for everyone!';
+}
